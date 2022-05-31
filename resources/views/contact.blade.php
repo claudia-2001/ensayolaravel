bf35cf311cd943433079ecb38fc47708de3492d6
claudia-2001 <claudia.abeleira@vigo.org>
2022-05-31 08:49:57 +0200

    ensayolaravel

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,5 +3,16 @@
 @section('content')
 
       <h1>Contenido de contactos</h1>
+<body>
+    <form action="">
+        <input type="text" name="nombre" class="nombre" placeholder="Nombres">
+        <input type="text" name="apellido" class="apellido" placeholder="Apellidos">
 
+        <input type="email" name="correo" class="correo" placeholder="Correo">
+        <input type="tel" name="telefono" class="telefono" placeholder="Telefono">
+        <textarea name="mensaje" id="" class="mensaje" placeholder="Mensaje:"></textarea>
+
+        <button name="enviar" id="boton" class="boton_envio">Enviar</button>
+    </form>
+</body>
 @endsection
